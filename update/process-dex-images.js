7dbb8f92adae283d9992195690d979afef7af701
tigt <tigt@mortropolis.com>
2021-12-29 13:19:51 -0500

    Add dex image processing

---
+++ update/process-dex-images.js
@@ -0,0 +1,47 @@
+const sharp = require('sharp')
+const crypto = require('crypto')
+const fs = require('fs/promises')
+const path = require('path')
+
+;(async function() {
+  const imageFiles = (await fs.readdir(`${__dirname}/img/`))
+    .filter(filename => filename.endsWith('.png')) // Avoid .DS_Store and other gunk files
+
+  const outputPromises = []
+  for (const filename of imageFiles) {
+    const pathIn = path.join(__dirname, '/img/', filename)
+    const pathOut = path.join(__dirname, '/../docs/dex/img/', filename).replace(/\.png$/, '.jpg')
+
+    outputPromises.push(fs.readFile(pathIn).then(bytes => {
+      const hash = crypto.createHash('md5')
+        .update(bytes)
+        .digest('base64')
+        .replace(/[/+=]/g, char => {
+          switch (char) {
+            case '/': return '_'
+            case '+': return '-'
+            default: return ''
+          }
+        })
+
+      return sharp(pathIn)
+        .resize({
+          width: 400,
+          withoutEnlargement: true
+        })
+        .blur(0.3) // Some original images have crust like whoah
+        .jpeg({ mozjpeg: true })
+        .toFile(pathOut)
+        .then(({ width, height }) => [filename, {
+          sizes: [width, height],
+          hash
+        }])
+    }))
+  }
+
+  const results = Object.fromEntries(await Promise.all(outputPromises))
+  await fs.writeFile(
+    `${__dirname}/../11ty/_data/dex-images.json`,
+    JSON.stringify(results, null, 2)
+  )
+})()
